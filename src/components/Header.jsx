e16f3efbe61a76cd4d8ba91054069ca03509eb5a
thegittyuser <webdevsserv@gmail.com>
2025-09-03 13:29:36 +0500

    Style Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,27 +2,34 @@ import "./css/header.css";
 
 function Header() {
   return (
-    <>
-      <section className="header">
-        <div className="logo-text">
-          <h2>WeBee Solutions</h2>
-        </div>
-        <nav className="navbar">
-          <ul>
-            <li>
-              <a href="#">Home</a>
-              <a href="#">About</a>
-              <a href="#">Skills</a>
-              <a href="#">Projects</a>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
-        <div className="contact-btn">
-          <button>Contact Me</button>
-        </div>
-      </section>
-    </>
+    <header className="header">
+      <div className="logo-text">
+        <h2>WeBee Solutions</h2>
+      </div>
+      <nav className="navbar" aria-label="Main Navigation">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="contact-btn">
+        <button>Contact Me</button>
+      </div>
+    </header>
   );
 }
+
 export default Header;
